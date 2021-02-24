1b908182c2463799eeacbd3804f81c27e540daba
FlorenCE <2571472753@qq.com>
2021-02-24 20:41:36 +0800

    uodate

--- study/Day01.py
+++ study/Day01.py
@@ -5,16 +5,10 @@ import this
 
 #turtle
 import turtle
-
+'''
 turtle.pensize(4)
 turtle.pencolor('red')
-
 turtle.forward(100)
 turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-turtle.right(90)
-turtle.forward(100)
-
 turtle.mainloop()
+'''
