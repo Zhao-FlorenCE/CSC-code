4f0cf46642e541fb9a134b2075f2e9bdd02e7443
FlorenCE <2571472753@qq.com>
2021-02-28 23:25:25 +0800

    2021/2/25 23:25

--- csc1001/Assignment1/q4.py
+++ csc1001/Assignment1/q4.py
@@ -1,8 +1,13 @@
-row = int(input('Input your row: '))
-
-if row > 0:
-    print('m\tm + 1\tm ** (m + 1)')
-    for i in range(row):
-        print('%d\t%d\t%d' % (i + 1, i + 2, (i + 1) ** (i + 2)))
-else:
-    print('Please input a positive nunber.')
+while True:
+    try:
+        row = int(input('Input your row: '))
+    except:
+        print('Please input an integer.')
+        continue
+    if row <= 0:
+        print('Please input a positive nunber.')
+        continue
+    else:
+        print('m\tm + 1\tm ** (m + 1)')
+        for i in range(row):
+            print('%d\t%d\t%d' % (i + 1, i + 2, (i + 1) ** (i + 2)))
