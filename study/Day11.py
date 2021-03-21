9e10fb32045602118d605f87bd72c8419be49c59
FlorenCE <2571472753@qq.com>
2021-03-21 21:05:31 +0800

    2021/3/21 21:05

--- study/Day11.py
+++ study/Day11.py
@@ -9,31 +9,22 @@ def main():
     time = str(datetime.date.today())
     text = open('C:/Users/FlorenCE/Desktop/News py/' \
                 + time + '.txt', 'w', encoding='utf-8')
-    #print('Daily News:')
     text.write('Daily News:' + '\n')
     resp1 = requests.get('http://api.tianapi.com/bulletin/index' \
                          + key1)
     data_model1 = json.loads(resp1.text)
     i = 1
     for news in data_model1['newslist']:
-        #print('%d.%s (%s)' % (i, news['title'], news['mtime']))
-        #print('  ', news['digest'])
         text.write(str(i) + '.' + news['title'] + ' (' + \
                    news['mtime'] + ')\n')
         text.write('  ' + news['digest'] + '\n' + '\n')
         i += 1
-    #print()
-    #print('Scientific Sailing:')
     text.write('\n' + 'Scientific Sailing:' + '\n')
     resp2 = requests.get('http://api.tianapi.com/sicprobe/index' \
                          + key2)
     data_model2 = json.loads(resp2.text)
     j = 1
     for news in data_model2['newslist']:
-        #print('%d.%s (%s)' % (j, news['title'], news['ctime']))
-        #print('  ', news['description'])
-        #print('(source: %s, url: %s)' % \
-        #     (news['source'], news['url']))
         text.write(str(j) + '.' + news['title'] + ' (' + \
                    news['ctime'] + ')\n')
         text.write('  ' + news['description'] + '\n')
